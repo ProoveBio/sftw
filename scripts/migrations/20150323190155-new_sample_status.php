a11acd5b7e586292d62ccd8aa26239c75c26e35f
Brian Imbach <bimbach@proovebio.com>
2015-03-23 12:19:09 -0700

    HEL-564: create migration to add new Sample Status and make status encounter_id nullable

---
+++ scripts/migrations/20150323190155-new_sample_status.php
@@ -0,0 +1,48 @@
+<?php
+
+use Dws\Sftw\Db\Schema\AbstractChange as SchemaChange;
+
+/**
+ * @author
+ */
+class MigrationClass_20150323190155 extends SchemaChange
+{
+	private $newSampleName = 'Batched Pending Intake';
+
+	public function up()
+	{
+		$sql1 = <<< EOT
+INSERT INTO tblSampleStatus
+	(sample_status_name)
+VALUES
+	('$this->newSampleName');		
+EOT;
+
+		$sql2 = <<< EOT
+ALTER TABLE tblSample
+	MODIFY encounter_id INT(11) UNSIGNED DEFAULT NULL;
+EOT;
+
+		$this->querySQL($sql1);	
+		$this->querySQL($sql2);	
+	}
+
+	public function down()
+	{
+		$sql1 = <<< EOT
+DELETE FROM tblSampleStatus WHERE sample_status_name = '$this->newSampleName';
+EOT;
+		$sql2 = <<< EOT
+ALTER TABLE tblSampleStatus AUTO_INCREMENT = 1; 
+EOT;
+
+		$sql3 = <<< EOT
+ALTER TABLE tblSample
+	MODIFY encounter_id INT(11) UNSIGNED NOT NULL;
+EOT;
+		$this->querySQL($sql1);
+		$this->querySQL($sql2);
+		$this->querySQL($sql3);
+	}
+
+}
